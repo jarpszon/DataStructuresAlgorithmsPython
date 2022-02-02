859081a1f6de272ad313f136ecb92b30f166c1a0
jarpszon <jpszonczak@gmail.com>
2022-02-02 10:29:16 +0100

    almost done

---
+++ LinkedList/LinkedList_FULL.py
@@ -0,0 +1,105 @@
+from hashlib import new
+from operator import ne
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)  
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1    
+        return True
+    
+    def pop(self):
+        # 0 nodes
+        if self.length == 0:
+            return None
+        # >= 1 node
+        temp = self.head
+        pre = self.head
+
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        
+        # 1 node
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp= temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return None
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index -1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+ 
+
+my_linked_list = LinkedList(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.print_list()
+my_linked_list.get(0)
+
+
+
+
--- LinkedList/LinkedList_constructor.py
+++ LinkedList/LinkedList_constructor.py
@@ -15,10 +15,19 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
-    
 
-my_linked_list = LinkedList(4)  
+    def append(self, value):
+        new_node = Node(value)  
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1    
+        return True
 
-print(my_linked_list.head.value)     
-print(my_linked_list.tail.next) 
-print(my_linked_list.length) 
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.print_list()
+  
